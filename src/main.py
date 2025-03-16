6b155adaaa459ce53e1438e2c658d20d83419236
Ilia Latauri <ilialatauri@Ilias-MacBook-Air.local>
2025-03-16 19:32:11 +0400

    update readme

--- src/main.py
+++ src/main.py
@@ -22,4 +22,4 @@ def compute_features(application_data: ApplicationData):
 
 
 if __name__ == '__main__':
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=False)
